b3f8344f1d7c3dca80aade11ec1ee1ce21e14a8c
hungqng <97866339+hungqng@users.noreply.github.com>
2022-02-10 13:29:16 -0800

    Add files via upload
    Solution

--- findNumber.js
+++ findNumber.js
@@ -2,6 +2,11 @@ arr = [0,1,2,3,4,5]
 k = 7;
 
 
-if (arr.includes(k)) {
-    return console.log("YES")
+function findNumber(arr, k) {
+    // Write your code here
+    if (arr.includes(k)){
+        return ("YES")
+    } else {
+        return ("NO")
+    }
 }
